395d9d47091b14d0f1ab79f78a6c8ee1c2586884
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-17 19:33:16 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Goku\nBot\nMD', {
+say('Nexus\nBot\nMD', {
 font: 'block',
 align: 'center',
 colors: ['magenta', 'cyan']
