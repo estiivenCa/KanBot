5502bb4368317c04380840c45ee036879b8a36a1
E D E R <139829285+WOTCHITO@users.noreply.github.com>
2024-04-10 17:42:13 -0500

    Update tw.js

--- plugins/tw.js
+++ plugins/tw.js
@@ -8,7 +8,7 @@ let msg = await generateWAMessageFromContent(m.chat, { locationMessage: {
   degreesLatitude: 0,
   degreesLongitude: 0,
   name: 'Github del bot',
-  address: 'By Jxtxn17',
+  address: 'By EDER',
   url: md,
   isLive: true,
   accuracyInMeters: 0,
@@ -21,8 +21,8 @@ let msg = await generateWAMessageFromContent(m.chat, { locationMessage: {
 conn.relayMessage(m.chat, msg.message, {})
 }
 
-//let str = '*🍧 BaileyBot-MD*'
-//await conn.sendUrl(m.chat, str, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen1, thumbnailUrl: imagen1, title: '🍓 Jxtxn17', }})
+//let str = '*🍧 NEXUS-MD*'
+//await conn.sendUrl(m.chat, str, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen1, thumbnailUrl: imagen1, title: 'EDER👑', }})
  
 handler.command = /^((a|a)a)$/i
 handler.owner = true
