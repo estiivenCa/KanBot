8ee7be3a2c06c07f4d9b2fb24dbb9e0d07e4332e
E D E R <139829285+WOTCHITO@users.noreply.github.com>
2024-04-10 19:04:28 -0500

    Update comandos-cursos.js

--- plugins/comandos-cursos.js
+++ plugins/comandos-cursos.js
@@ -8,7 +8,7 @@ _${usedPrefix}ytmp4_`.trim();
 
   if (command == "cursos") {
     var play = `*[ CURSOS ]*\n\n*Mini cursos de programación*\n
-*Entra al numero +1 (720) 263-5863*\n*en el perfil hay cursos básicos de programación*\n\n*aparte si tienes dudas mande mensaje ahi, o al numero +542215034412*\n\n¡¡Disfruta de los mini cursos!!`.trim();
+*Entra al canal de cursos y guias gratis *\n*en el canal hay cursos básicos de programación*\n\n*aparte si tienes dudas mande mensaje al creador, o al numero +573027866596*\n\n¡¡Disfruta de los mini cursos!!`.trim();
     m.reply(play);
   }
 };
