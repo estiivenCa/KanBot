38efbfb8d834bc1bb80c1b11c2448d6206683cbb
E D E R <139829285+WOTCHITO@users.noreply.github.com>
2024-04-19 14:08:40 -0500

    Update comandos-cursos.js

--- plugins/comandos-cursos.js
+++ plugins/comandos-cursos.js
@@ -8,7 +8,7 @@ _${usedPrefix}ytmp4_`.trim();
 
   if (command == "cursos") {
     var play = `*[ CURSOS ]*\n\n*Mini cursos de programación*\n
-\n*en el canal hay cursos básicos de programación*\n\n*https://whatsapp.com/channel/0029VaUzrr0HLHQU93Ig2t3I  aparte si tienes dudas mande mensaje al creador, o al numero +573027866596*\n\n¡¡Disfruta de los mini cursos!!`.trim();
+\n*en el canal hay cursos básicos de programación*\n\n*aparte si tienes dudas mande mensaje al creador, o al numero +573027866596*\n\n¡¡Disfruta de los minicursos  https://whatsapp.com/channel/0029VaUzrr0HLHQU93Ig2t3I!!`.trim();
     m.reply(play);
   }
 };
