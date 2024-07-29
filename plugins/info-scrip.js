5abf9f28e35110dfd5ca64be55385b66243f3a76
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-07-29 12:08:48 -0500

    Update info-scrip.js

--- plugins/info-scrip.js
+++ plugins/info-scrip.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix }) => {
 let git = ' ꨄ *DUEÑO* * ⁨𝑺̳̽𝒕̳̽𝒊̳𝒊̳𝒗̳̽𝒙̳̽𝒏̳̽×፝֟͜×*\n\nꨄ *CONTACTO* *wa.me/5492612357997*'
-await conn.sendUrl(m.chat, git, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen2, thumbnailUrl: imagen1, title: '\t\t\t\t\t\t✰ 𝙺𝚊𝚗𝙱𝚘𝚝 ✰', }})
+await conn.sendUrl(m.chat, git, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen2, title: '\t\t\t\t\t\t✰ 𝙺𝚊𝚗𝙱𝚘𝚝 ✰', }})
  
 }
 handler.tags =['info'] 
