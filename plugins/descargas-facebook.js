4eddd79df37f091a46b3b8d08ad8ee0e5ff3046c
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-07-29 13:08:02 -0500

    Update descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -7,16 +7,16 @@ let vid
 const isCommand7 = /^(facebook|fb|facebookdl|fbdl)$/i.test(command)
 
 async function reportError(e) {
-await conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m, fake, )
+await conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m,  )
 console.log(`🚩 ERROR EN: ${usedPrefix + command} ⚠️\n`)
 console.log(e)
 }
   
 switch (true) {   
 case isCommand7:
-if (!text) return conn.reply(m.chat, `🎌 *Ingrese un enlace de facebook*\n\nEjemplo, !fb https://fb.watch/kAOXy3wf2L/?mibextid=Nif5oz`, m, fake, )
-if (!args[0].match(/www.facebook.com|fb.watch|web.facebook.com|business.facebook.com|video.fb.com/g)) return conn.reply(m.chat, '🎌 *No es un enlace válido*', m, fake, )
-await conn.reply(m.chat, '⏰ Espere un momento', m, fake, )
+if (!text) return conn.reply(m.chat, `🎌 *Ingrese un enlace de facebook*\n\nEjemplo, !fb https://fb.watch/kAOXy3wf2L/?mibextid=Nif5oz`, m,  )
+if (!args[0].match(/www.facebook.com|fb.watch|web.facebook.com|business.facebook.com|video.fb.com/g)) return conn.reply(m.chat, '🎌 *No es un enlace válido*', m,  )
+await conn.reply(m.chat, '⏰ Espere un momento', m,  )
 m.react(done)
 let messageType = checkMessageType(args[0])
 let message = ''
