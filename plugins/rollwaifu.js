5def0e3d9975b58975da13ece542be5ee8722af5
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-31 08:48:36 -0500

    Actualizar rollwaifu.js

--- plugins/rollwaifu.js
+++ plugins/rollwaifu.js
@@ -24,31 +24,10 @@ function mostrarWaifu(waifu) {
 // Ejecuta la función para obtener y mostrar una waifu aleatoria
 const waifuAleatoria = obtenerWaifuAleatoria();
 mostrarWaifu(waifuAleatoria);
-};
+/*};
 handler.help = ['rollwaifu'];
 handler.tags = ['anime'];
 handler.command = ['rw', 'rollwaifu'];
 handler.register = true;
 
-export default handler;
-
- function clockString(ms) { 
-   let h = Math.floor(ms / 3600000) 
-   let m = Math.floor(ms / 60000) % 60 
-   let s = Math.floor(ms / 1000) % 60 
-   console.log({ms,h,m,s}) 
-   return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':') 
- } 
-
- /*function msToTime(duration) { 
- var milliseconds = parseInt((duration % 1000) / 100), 
- seconds = Math.floor((duration / 1000) % 60), 
- minutes = Math.floor((duration / (1000 * 60)) % 60), 
- hours = Math.floor((duration / (1000 * 60 * 60)) % 24) 
-  
- hours = (hours < 10) ? "0" + hours : hours 
- minutes = (minutes < 10) ? "0" + minutes : minutes 
- seconds = (seconds < 10) ? "0" + seconds : seconds 
-  
- return minutes + " m y " + seconds + " s "  
- }  */
+export default handler;*/
