54cece4426ba8becd196deaa950d88bde2d57aa6
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-08-02 13:17:52 -0500

    Update descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -1,29 +1,24 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) return m.reply('*`Ingresa un enlace de Facebook`*');
+  if (!args[0]) return m.reply('*`Ingresa un enlace de facebook`*');
 
   try {
-    await m.react('🕓'); 
+await m.react('🕓'); 
     const apiResponse = await fetch(`https://thepapusteam.koyeb.app/api/fbvideodownload?url=${args[0]}`);
     const responseData = await apiResponse.json();
     
     if (responseData.success) {
-      const { creator, title, src_url, og_url, picture, links, r_id } = responseData;
+      const { title, links } = responseData;
       let txt = '    `Facebook downloader`\n\n';
-      txt += `> *Título*: _${title}_\n`;
-      txt += `> *Enlace de origen*: ${src_url}\n`;
-      txt += `> *Enlace OG*: ${og_url}\n`;
-      txt += `> *ID*: ${r_id}\n`;
-      txt += `> *Imagen*: ${picture}\n`;
+      txt += `> *Titulo* : _${title}_\n`;
 
       await conn.sendMessage(m.chat, { video: { url: links[0].link }, caption: txt }, { quoted: m });
-      await m.react('✅'); 
+await m.react('✅'); 
     } else {
-      await m.react('❌'); 
     }
   } catch {
-    await m.react('❌'); 
+await m.react('❌'); 
   }
 }
 
