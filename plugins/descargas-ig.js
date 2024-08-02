06c532b81687f44a7701520f293a182aa4fc3dd5
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-08-02 12:17:46 -0500

    Update descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -1,22 +1,43 @@
+import fetch from 'node-fetch';
+import axios from 'axios';
+import instagramGetUrl from 'instagram-url-direct';
+import { instagram } from '@xct007/frieren-scraper';
+import { instagramdl } from '@bochilteam/scraper';
 import Scraper from '@SumiFX/Scraper';
 
 const handler = async (m, { conn, args, usedPrefix, command }) => {
-  // Mensajes de proceso y error
-  const waitMessage = '🍁 *Descargando su video de Instagram*';
+  const waitMessage = '🍁 *Descargando su contenido...*';
   const errorMessage = '🍁 Ocurrió un error inesperado.';
+  const videoCaption = `✅️ *Su Video De Instagram*`; // Reemplaza con la descripción deseada
+  const packname = 'TuPackName'; // Reemplaza con el nombre de tu paquete
+  const icons = 'https://example.com/thumbnail.jpg'; // Reemplaza con la URL de tu imagen en miniatura
+  const channel = 'https://example.com'; // Reemplaza con la URL de tu canal
 
-  // Verifica si se ha proporcionado un enlace
+
   if (!args[0]) return m.reply('🍁 Ingresa un enlace de Instagram.');
 
-  try {
-    // Envía un mensaje de espera
-    conn.reply(m.chat, waitMessage, m);
+  // Envía un mensaje de espera con información de contexto
+  conn.reply(m.chat, waitMessage, m, {
+    contextInfo: {
+      externalAdReply: {
+        mediaUrl: null,
+        mediaType: 1,
+        showAdAttribution: true,
+        title: packname,
+        body: wm,
+        previewType: 0,
+        thumbnail: icons,
+        sourceUrl: channel
+      }
+    }
+  });
 
-    // Obtiene la URL de descarga del video
+  try {
+    // Intenta obtener la URL de descarga del video usando Scraper
     let { dl_url } = await Scraper.igdl(args[0]);
 
     // Envía el video descargado al chat
-    await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: '✅️ *Su Video De Instagram*' });
+    await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: videoCaption }, { quoted: fkontak });
   } catch (e) {
     console.log(e);
     m.reply(errorMessage);
@@ -25,7 +46,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
 
 handler.help = ['ig <enlace>'];
 handler.tags = ['descargas'];
-handler.command =/^(instagram|ig(dl)?)$/i
+handler.command = ['ig', 'instagram'];
 handler.register = true;
 handler.limit = 1;
 
