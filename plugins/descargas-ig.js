341e5cee8d334b3721b06e46952a5393ffd9edec
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-08-02 11:08:46 -0500

    Update descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -4,12 +4,9 @@ if (!args[0]) return m.reply('🍁 Ingresa un enlace de Instagram.')
 try {
 conn.reply(m.chat, '🍁*Descargando su video de Instagram*', m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
+ }}})
 let { dl_url } = await Scraper.igdl(args[0])
-await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `✅️ *Su Video De Instagram*\n${botname}` }, { quoted: fkontak})
+await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `✅️ *Su Video De Instagram*` }, { quoted: fkontak})
 } catch (e) {
   console.log(e)
   m.reply('🍁  Ocurrió un error inesperado.')
