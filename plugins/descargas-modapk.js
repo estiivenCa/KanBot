574b7ed8dc79674f0fa089cab2df579fd732acc1
Estiiven <122229140+estiivenCa@users.noreply.github.com>
2024-08-05 14:06:27 -0500

    Update descargas-modapk.js

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -5,7 +5,7 @@ import { search, download } from 'aptoide-scraper'
  if (!text) return conn.reply(m.chat, '🎌 *Ingrese el nombre de la apk que esta buscando*', m, fake, ) 
   
  try { 
-  
+     await m.react('🔄')
  let searchA = await search(text) 
  let data5 = await download(searchA[0].id) 
  let response = `💌 *Nombre:* ${data5.name}\n📦 *Paquete:* ${data5.package}\n🕒 *Actualización:* ${data5.lastup}\n📥 *Tamaño:* ${data5.size}` 
@@ -13,10 +13,13 @@ import { search, download } from 'aptoide-scraper'
   
   if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) { 
  return await conn.reply(m.chat, '🚩 *El archivo es demaciado pesado*', m, fake, ) 
+     return await m.react('❌')
  } 
  await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m}) 
+  await m.react('✅') 
  } catch { 
  return conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, fake, ) 
+   await m.react('❌')
  }     
  } 
   
